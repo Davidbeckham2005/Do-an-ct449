7c6eac6f55c2af0379f0d410ccff8bc428e77e68
davidbeckham2005 <Dinhhoangkhamtb04@gmail.com>
2025-10-01 10:40:06 +0700

    them file controller va routes

---
+++ app/controllers/contact.controllers.js
@@ -0,0 +1,21 @@
+exports.create = (req, res) => {
+    res.send("create handler");
+};
+exports.findAll = (req, res) => {
+    res.send("findAll handler");
+};
+exports.finOne = (req, res) => {
+    res.send("finOne handler");
+};
+exports.update = (req, res) => {
+    res.send("update handler");
+};
+exports.delete = (req, res) => {
+    res.send("delete handler");
+};
+exports.deleteAll = (req, res) => {
+    res.send("deleteAll handler");
+};
+exports.findAllFavorite = (req, res) => {
+    res.send("create handler");
+};
