04f2e8e532e53d0dc52cd80eb84150d8c808a9b1
davidbeckham2005 <Dinhhoangkhamtb04@gmail.com>
2025-10-01 10:35:07 +0700

    thay doi vi them controller và routers

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const contactRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to contact book application");
 });
+app.use("/api/contacts", contactRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require("./app");
-const config = require("./app/config");
+const app = require("./app.js"); //nap file app.js
+const config = require("./app/config"); //nap file index.js
 const port = config.app.port;
 
 app.listen(port, () => {
